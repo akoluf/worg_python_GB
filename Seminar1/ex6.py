48297a22858bf11b3e87bfe6a535b9276f8aad6d
akoluf <Rifald478@gmail.com>
2023-10-15 19:54:31 +0300

    обновил решение задачи 6

--- Seminar1/ex6.py
+++ Seminar1/ex6.py
@@ -5,22 +5,24 @@
 # 385916 – счастливый, т.к. 3+8+5=9+1+6. Вам требуется написать
 # программу, которая проверяет счастливость билета.
 
-ticket_number = input("Введите номер билета (6 цифр): ")
+n = input("Введите номер билета (6 цифр): ")
 
-if len(ticket_number) != 6:  # проверка на длину номера билета
+n = str(n)  # преобразуем число в строку
+
+if len(n) != 6:  # проверка на длину номера билета
     print("Номер билета должен состоять из 6 цифр.")
 else:
-    digit1 = int(ticket_number[0])
-    digit2 = int(ticket_number[1])
-    digit3 = int(ticket_number[2])
-    digit4 = int(ticket_number[3])
-    digit5 = int(ticket_number[4])
-    digit6 = int(ticket_number[5])
+    digit1 = int(n[0])
+    digit2 = int(n[1])
+    digit3 = int(n[2])
+    digit4 = int(n[3])
+    digit5 = int(n[4])
+    digit6 = int(n[5])
 
     sum1 = digit1 + digit2 + digit3
     sum2 = digit4 + digit5 + digit6
 
     if sum1 == sum2:
-        print("Поздравляем, вам попался счастливый билет!")
+        print("yes")
     else:
-        print("Увы, билет не счастливый.")
+        print("no")
