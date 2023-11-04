e6ab160c13defb04b5fc33f56e520ae4cd0e9438
akoluf <Rifald478@gmail.com>
2023-11-04 16:01:34 +0300

    добавил решение задачи 30

---
+++ Seminar6/ex30.py
@@ -0,0 +1,20 @@
+# Задача 30: Заполните массив элементами арифметической
+# прогрессии. Её первый элемент, разность и количество
+# элементов нужно ввести с клавиатуры. Формула для
+# получения n-го члена прогрессии: an = a1 + (n-1) * d.
+# Каждое число вводится с новой строки.
+
+def arithmetic_progression(a1, d, n):
+    progression = []
+    for i in range(n):
+        progression.append(a1 + i * d)
+    return progression
+
+a1 = int(input("Введите первый элемент прогрессии: "))
+d = int(input("Введите разность прогрессии: "))
+n = int(input("Введите количество элементов прогрессии: "))
+
+progression = arithmetic_progression(a1, d, n)
+
+for element in progression:
+    print(element)
