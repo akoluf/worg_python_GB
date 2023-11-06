9c454bc1416cdf0a14f3927ebe571f5c714b02b7
akoluf <Rifald478@gmail.com>
2023-11-06 18:33:32 +0300

    подправил задачу 36

--- Seminar7/ex36.py
+++ Seminar7/ex36.py
@@ -6,7 +6,7 @@
 # ровно два аргумента, как, например, у операции умножения.
 # print_operation_table(lambda x, y: x * y)
 
-def print_operation_table(operation, num_rows=9, num_columns=9):
+def print_operation_table(operation, num_rows=6, num_columns=6):
     for i in range(1, num_rows+1):
         for j in range(1, num_columns+1):
             result = operation(i, j)
